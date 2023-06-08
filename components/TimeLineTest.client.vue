1e5d4f98d2fea4927f4bddbc96efecb0ef5bb84c
H171 <1091088708@qq.com>
2023-06-08 13:27:11 +0800

    fix: framework timeline

--- components/TimeLineTest.client.vue
+++ components/TimeLineTest.client.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import { Timeline } from '@knight-lab/timelinejs'
-import { Data } from '~/types/index'
 import '@knight-lab/timelinejs/dist/css/timeline.css'
-import { data } from '~/data/timeline'
+import { data, options } from '~/data/timeline'
 
 // const slides = ref<TimelineJSSlide[]>(data)
 const timelineEmbed = ref<HTMLElement | null>(null)
 const timelineInstance = ref<Timeline | null>(null)
-useNuxtApp().hook('app:mounted', () => {
-  timelineInstance.value = new Timeline('timeline-embed', data)
+onNuxtReady(() => {
+  timelineInstance.value = new Timeline('timeline-embed', data, options)
 })
 </script>
 
--- data/timeline.ts
+++ data/timeline.ts
@@ -1,45 +1,58 @@
-// import type { Props } from '../types/timelinejs3'
+import type { Data, Options } from '../types'
 
-export const data = {
-  title: {
-    media: {
-      url: 'http://www.germuska.com/salzburg-album/full/2QVB_022.jpg',
-      caption: 'The secret passage at Schloss Leopoldskron, Salzburg, Austria',
-      credit: 'Joe Germuska',
+export const data: Data = {
+  // title: {
+  //   start_date: {
+  //     year: 2020,
+  //     month: 1,
+  //     day: 1,
+
+  //   },
+  //   text: {
+  //     headline: 'TimelineJS Media Types',
+  //     text: '<p>TimelineJS supports many media types. This provides a simple way to test them all. This page tests a simple image on a remote server.</p>',
+  //   },
+  // },
+  events: [
+    {
+      start_date: {
+        year: 2020,
+        month: 1,
+        day: 1,
+      },
     },
-    text: {
-      headline: 'TimelineJS Media Types',
-      text: '<p>TimelineJS supports many media types. This provides a simple way to test them all. This page tests a simple image on a remote server.</p>',
-    },
-  },
-  events: [{
-    id: 'youtube',
-    media: {
-      url: 'https://www.youtube.com/watch?v=pi2v1m6gmD8&t=5m21s',
-      caption: 'the Monkey section of David Van Tieghem\'s <em>Ear Drums</em>',
-    },
-    start_date: {
-      year: '1901',
-    },
-    text: {
-      headline: 'YouTube Videos',
-      text: '<p>if Timeline finds a URL that starts with \'youtube.com\' or \'youtu.be\', it will try to use it to embed a YouTube video. The <em>protocol</em> part of the URL (e.g. <em>https://</em>) is technically optional.</p><p>You can start at a specific point in the video using the <code>t=#m#s</code> parameter: see <a href=\'http://youtubetime.com/\'>http://youtubetime.com/</a></p>',
-    },
-  },
-  {
-    id: 'vimeo',
-    media: {
-      url: 'http://vimeo.com/20839673',
-      caption: '<em>Phat Tai</em>, a story of Vietnamese fishermen on the Gulf Coast.',
-      credit: 'Joe York/Southern Foodways Alliance',
-    },
-    start_date: {
-      year: '1902',
-    },
-    text: {
-      headline: 'Vimeo',
-      text: 'To embed a Vimeo video, just copy the URL of the Vimeo.com page which shows the video.',
-    },
-  },
   ],
+  eras: [
+    {
+      start_date: {
+        year: 2020,
+        month: 1,
+        day: 1,
+      },
+      end_date: {
+        year: 2020,
+        month: 5,
+        day: 1,
+      },
+    },
+    {
+      start_date: {
+        year: 2020,
+        month: 6,
+        day: 10,
+      },
+      end_date: {
+        year: 2020,
+        month: 7,
+        day: 1,
+      },
+    },
+  ],
+}
+
+export const options: Options = {
+  height: 100,
+  language: 'zh-cn',
+  timenav_position: 'top',
+
 }
